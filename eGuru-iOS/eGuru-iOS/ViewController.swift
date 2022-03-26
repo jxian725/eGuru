a97f412fb9ad925c5d046c72c50aa48c8c50ed62
JXian <jingxian_98@hotmail.com>
2022-03-26 18:11:08 +0800

    init iOS App

---
+++ eGuru-iOS/eGuru-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eGuru-iOS
+//
+//  Created by UHS TECHNOLOGY on 26/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
